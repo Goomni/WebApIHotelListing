22e99fb0df0ef3b15444e883d87a66e29efe18e1
DESKTOP-0OBN2F0\rnals <gumin.chung@dataknows.ai>
2021-11-23 15:32:13 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting;
+using System;
+using System.IO;
+
+namespace WebApIHotelListing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().
+                         WriteTo.
+                         File(path : Path.Combine(Directory.GetCurrentDirectory(), "hotelListingLog.txt"),
+                              outputTemplate : "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                              rollingInterval : RollingInterval.Day,
+                              restrictedToMinimumLevel : Serilog.Events.LogEventLevel.Information).
+                         CreateLogger();
+
+            try
+            {
+                Log.Information("Application is Starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
